ebe1e8ed266afe3a417eaf0776309f8c0831d3dc
RamyaSprakash <ramyaprakash375@gmail.com>
2024-10-19 20:30:50 +0530

    Change

--- src/sorting/Demo.java
+++ src/sorting/Demo.java
@@ -3,9 +3,6 @@ import java.util.*;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		double amount=sc.nextDouble();
+		System.out.println("Hello");
 	}
-
 }
